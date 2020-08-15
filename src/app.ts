81bc731e2186e2df61b9d01aae3b2d793044a76b
tsubasa1122 <nakanot@open8.com>
2020-08-15 19:54:14 +0900

    typeを細かく設定

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+enum ProjectStatus {
+  Active, Finished
+}
+
+class Project {
+  constructor(public id: string, public title: string, public description: string, public monday: number, public status: ProjectStatus) {
+
+  }
+}
 // Project State Management
+type Listner = (items: Project[]) => void;
 class ProjectState {
-  private listners: any[] = [];
-  private projects: any[] = [];
+  private listners: Listner[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {
@@ -15,17 +25,19 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listner) {
     this.listners.push(listenerFn);
   }
 
   addProject(title: string, description: string, manday: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      manday: manday,
-    }
+    const newProject = new Project(
+      Math.random().toString(),
+      title,
+      description,
+      manday,
+      ProjectStatus.Active
+    );
+
     this.projects.push(newProject);
     for (const listenerFn of this.listners) {
       listenerFn(this.projects.slice());
@@ -81,7 +93,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
 
   constructor(private type: 'active' | 'finished') {
     this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
@@ -92,7 +104,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedProjects = projects;
       this.renderProjects();
     });
