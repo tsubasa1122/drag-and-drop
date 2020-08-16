2430f96bb7c23dbfd144430aaf117bbea033cd5b
tsubasa1122 <nakanot@open8.com>
2020-08-16 16:54:37 +0900

    フォルダ整理

---
+++ src/state/project-state.ts
@@ -0,0 +1,57 @@
+namespace App {
+  type Listner<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listners: Listner<T>[] = [];
+
+    addListener(listenerFn: Listner<T>) {
+      this.listners.push(listenerFn);
+    }
+
+  }
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    addProject(title: string, description: string, manday: number) {
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        manday,
+        ProjectStatus.Active
+      );
+
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find(prj => prj.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
